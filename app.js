e6f403b067cef6e036d7f5a30a376e5f86673385
Maria Waslick <mariawaslick@gmail.com>
2020-10-08 19:41:49 -0400

    got json posts working

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs")
 
-let notesData = require("./db/db.json")
-
 // Sets up the Express App
 // =============================================================
 const app = express();
@@ -15,16 +13,42 @@ const PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+let notesData = fs.readFileSync(path.join(__dirname, "/db/db.json"), "utf-8");
+notesData = JSON.parse(notesData)
+
 // Get Functions
 
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "/public/index.html"));
+app.get("*", function(req,res) {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
+app.get("/api/notes", function(req, res) {
+  res.json(notesData)
+});
+
+app.post("/api/notes", function(req, res) {
+  const newNote = {
+    title: req.body.title,
+    text: req.body.text
+  }
+
+  newNote.id = notesData.length + 1
+
+  console.log(newNote);
+
+  notesData.push(newNote);
+
+  const newNoteState = notesData
+
+  fs.writeFileSync(path.join(__dirname, "/db/db.json"), JSON.stringify(newNoteState));
+
+  res.json(notesData)
+})
+ 
 
 // Starts the server to begin listening
 app.listen(PORT, function() {

951e3335e2b5251f14af8f527a071ff23d6fbfd0
Maria Waslick <mariawaslick@gmail.com>
2020-10-08 20:34:13 -0400

    got app fully working, wrote delete function

--- app.js
+++ app.js
@@ -12,24 +12,23 @@ const PORT = 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"))
 
+// Reads the db.JSON file and parses it for the notes data variable
 let notesData = fs.readFileSync(path.join(__dirname, "/db/db.json"), "utf-8");
 notesData = JSON.parse(notesData)
 
-// Get Functions
-
-app.get("*", function(req,res) {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
-});
-
+// HTML Route for viewing the notes code
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
+// API route for getting the notes json file
 app.get("/api/notes", function(req, res) {
   res.json(notesData)
 });
 
+// API route for posting notes
 app.post("/api/notes", function(req, res) {
   const newNote = {
     title: req.body.title,
@@ -48,6 +47,22 @@ app.post("/api/notes", function(req, res) {
 
   res.json(notesData)
 })
+
+// API route for deleting notes
+app.delete("/api/notes/:id", function(req, res) {
+  const deletedNote = req.params.id;
+
+  const newnoteData = notesData.filter(note => note.id != deletedNote);
+
+  fs.writeFileSync(path.join(__dirname, "/db/db.json"), JSON.stringify(newnoteData));
+
+  res.json(newnoteData)
+})
+
+// API route for all other paths
+app.get("*", function(req,res) {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
  
 
 // Starts the server to begin listening
